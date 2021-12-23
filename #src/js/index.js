97fb697d295fb6fe3b002dde7b53f8f7be47a652
mortal <seregagl1996@gmail.com>
2021-12-23 10:13:02 +0300

    small fix

--- #src/js/index.js
+++ #src/js/index.js
@@ -1,6 +1,5 @@
 @@include('components/body-lock.js')
 @@include('components/document-click.js')
-@@include('components/slider.js')
 @@include('components/license-card.js')
 @@include('components/select.js')
 @@include('components/shadow.js')
--- copytrade/js/script.js
+++ copytrade/js/script.js
@@ -122,54 +122,6 @@ function documentActions(e) {
     }
     // NOTIFICATION END
 }
-const eventsSlider = document.querySelectorAll('.recent-events__swiper .swiper');
-
-if (eventsSlider.length > 0) {
-    new Swiper('.recent-events__swiper .swiper', {
-        slidesPerView: 1.5,
-        spaceBetween: 15,
-        watchOverflow: true,
-
-        navigation: {
-            prevEl: '.recent-events__swiper .swiper__button-prev',
-            nextEl: '.recent-events__swiper .swiper__button-next',
-        },
-
-        on: {
-            lock: function () {
-                this.el.classList.add('_lock');
-            },
-
-            init: function() {
-                this.el.classList.add('_init');
-            }
-        },
-
-        breakpoints: {
-            450: {
-                slidesPerView: 2,
-            },
-            576: {
-                slidesPerView: 3,
-            },
-            768: {
-                slidesPerView: 2,
-            },
-            1200: {
-                slidesPerView: 3,
-                spaceBetween: 40,
-            },
-            1920: {
-                slidesPerView: 4,
-                spaceBetween: 40,
-            },
-            2560: {
-                slidesPerView: 5,
-                spaceBetween: 40,
-            },
-        },
-    })
-}
 // LICENSE-CARD HEADER HEIGHT START
 let licenseCardHeading = document.querySelectorAll('.license-card__heading');
 
